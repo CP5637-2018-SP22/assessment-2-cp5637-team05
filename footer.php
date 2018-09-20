b1fe3acaf8a8e1056ebe2ca1705b2dbfeb33e932
jc455953 <ramandeepsingh.dhillon@my.jcu.edu.au>
2018-09-21 09:19:58 +1000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+			<p>U3W School</p>
+			<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+		</footer>
+
+	</div><!-- .container -->
+
+	<?php wp_footer(); ?>
+
+	</body>
+</html>
